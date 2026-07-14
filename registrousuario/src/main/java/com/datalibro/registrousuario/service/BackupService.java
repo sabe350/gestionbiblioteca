f0856c73f1ba47aa7a83cd9d721f407010b586df
sabe350 <si.bauer@duocuc.cl>
2026-07-13 23:57:17 -0400

    Update BackupService.java
    cambio de base de datos a _dev

--- registrousuario/src/main/java/com/datalibro/registrousuario/service/BackupService.java
+++ registrousuario/src/main/java/com/datalibro/registrousuario/service/BackupService.java
@@ -7,7 +7,7 @@ public class BackupService {
 
     public void createBackup(){
         String dumpPath = "C:\\laragon\\bin\\mysql\\mysql-8.4.3-winx64\\bin\\mysqldump.exe";
-        String dbName = "db_bibliotech_usuario";
+        String dbName = "db_bibliotech_usuario_dev";
         String dbUser = "root";
         String dbPass = "";
         String savePath = "C:\\backups\\backup_usuario_services.sql";
